ff61d04b0d780aa413d2881ebc72ef4fdad6c985
oscarmakesthings <106422937+oscarmakesthings@users.noreply.github.com>
2022-10-01 11:05:50 -0700

    Breadcrumbs

--- src/features/display/CampsiteDetailPage.js
+++ src/features/display/CampsiteDetailPage.js
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import { selectCampsiteById } from "../campsites/campsitesSlice";
 import { CampsiteDetail } from "../campsites/CampsiteDetail";
 import CommentsList from "../comments/CommentsList";
+import SubHeader from '../components/SubHeader';
 
 const CampsiteDetailPage = () => {
     const { campsiteId } = useParams();
     return (
         <Container>
+            <SubHeader current={campsite.name} detail={true} />
             <Row>
                 <CampsiteDetail campsite={campsite} />
                 <CommentsList campsiteId={campsiteId} />
