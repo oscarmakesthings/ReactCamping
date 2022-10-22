07949ed52c8b9ffc7198848ac48f4e03c4ef6e35
oscarmakesthings <106422937+oscarmakesthings@users.noreply.github.com>
2022-10-21 17:25:56 -0700

    AsyncPromotions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import Footer from './components/Footer';
@@ -17,6 +18,7 @@ function App() {
   useEffect(() => {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
+    dispatch(fetchPromotions());
 }, [dispatch]);
   return (
     <div className="App">
